0c72100ea7f0d1d90dfa6eb3222c0a04138eb875
Jonathan Regeimbal <jonathan@regeimbal.net>
2022-12-12 21:00:56 -0500

    Added terminal echos

--- setup/bootstrap-primary.sh
+++ setup/bootstrap-primary.sh
@@ -5,19 +5,23 @@ OLD_HOSTNAME=$1   #https://github.com/armbian/community/releases/download/202249
 NEW_HOSTNAME=$2   #/dev/disk2
 
 # Set hostname
+echo "Changin hostname $OLD_HOSTNAME > $NEW_HOSTNAME"
 sudo hostnamectl set-hostname $NEW_HOSTNAME
 sudo perl -i -p -e "s/$OLD_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
 
 # Install docker and start docker
+echo "Installing Docker"
 sudo apt install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 
 # Install K3s
+echo "Installing K3s for Primary"
 sudo curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.25.4+k3s1 sh -
 
 # Check status K3s
 sudo kubectl get nodes
 
 # Print token for use on workers
+echo "Your K3s Node Token /var/lib/rancher/k3s/server/node-token:"
 sudo cat /var/lib/rancher/k3s/server/node-token
--- setup/bootstrap-worker.sh
+++ setup/bootstrap-worker.sh
@@ -7,13 +7,16 @@ K3S_URL=$3        #EG: https://homelab-primary:6443
 K3S_TOKEN=$4      #EG: K10e12e687cc3bd959eb05f011addb1d5d05d454ba98015b576e12de62c12fc98dc::server:57f669d7737f97bfdd2db1d912b647b3
 
 # Set hostname
+echo "Changin hostname $OLD_HOSTNAME > $NEW_HOSTNAME"
 sudo hostnamectl set-hostname $NEW_HOSTNAME
 sudo perl -i -p -e "s/$OLD_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
 
 # Install docker and start docker
+echo "Installing Docker"
 sudo apt install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 
 # Install as worker node
+echo "Installing K3s for Worker"
 sudo curl -sfL https://get.k3s.io | K3S_URL=$K3S_URL K3S_TOKEN=$K3S_TOKEN INSTALL_K3S_VERSION=v1.25.4+k3s1 sh -
